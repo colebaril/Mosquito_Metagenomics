4129d0325d17d46508345d6c7d6019fb100e09ce
Cole Baril <cole@colebaril.ca>
2023-06-18 13:15:12 -0500

    Add files via upload

--- Scripts/metadata.R
+++ Scripts/metadata.R
@@ -25,29 +25,77 @@ write.csv(metadata, here("Data/metadata_cleaned.csv"))
 # PLOT ----
 metadata_plot <- metadata %>% 
   ggplot(aes(x = total_reads_log, y = reorder(id, total_reads_log))) + 
+  geom_segment(aes(x = number_reads_passing_host_filters_log, xend = total_reads_log, yend = id),
+               col = "black", linewidth = 1) +
   geom_point(aes(colour = species, shape = year, size = 1.2)) +
+  geom_point(aes(x = number_reads_passing_qc_log, colour = species, shape = year, size = 1.2)) +
+  geom_point(aes(x = number_reads_passing_host_filters_log, colour = species, shape = year, size = 1.2)) +
+  
   geom_text(aes(label = number_in_pool), hjust = -0.75) +
-  geom_point(aes(x = number_reads_passing_qc_log)) +
   theme_bw() +
   scale_colour_viridis_d("Species", labels = c("*Ae. canadensis*", "*Ae. vexans*", "*An. earlei*", "*Cq. perturbans*", 
                                                "*Cx. tarsalis*", "*Oc. dorsalis*", "*Oc. flavescens*", "*Oc. triseriatus*")) +
   scale_shape_manual("Year", values = c(15, 17, 18, 19)) +
   theme(panel.grid.major.x  = element_blank(),
         panel.grid.minor.x  = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
         legend.title = element_text(size = 12),
         legend.text = element_markdown(),
-        legend.position = c(0.9, 0.3),
-        legend.background = element_rect(linetype = 2, size = 0.5, colour = 1)) +
+        legend.position = c(0.95, 0.3),
+        legend.background = element_rect(linetype = 2, linewidth = 0.5, colour = 1)) +
   guides(colour = guide_legend(override.aes = list(size = 5)),
          shape = guide_legend(override.aes = list(size = 5)),
          size = "none") +
   labs(x = "Total Reads (Log 10)",
-       y = "Sample ID")
+       y = "Sample ID",
+       title = "Reads remaining after quality control and host-filtering (Mosquito) steps",
+       subtitle = "Number of specimens represented by each sample displayed to the right")
+
+
 
 # SAVE PLOT ----
 
 # Saving as a .tiff at 300 dpi. 
 
-tiff(here("Figures/metadata_plot.tiff"), units="in", width=14, height=10, res=300)
+tiff(here("Figures/metadata_plot.tiff"), units="in", width=16, height=10, res=300)
 metadata_plot
+dev.off()
+
+# PLOT ----
+host_filters_plot <- metadata %>% 
+  ggplot(aes(x = number_reads_passing_qc_log, y = reorder(id, number_reads_passing_qc_log))) + 
+  geom_segment(aes(x = number_reads_passing_host_filters_log, xend = number_reads_passing_qc_log, yend = id),
+               col = "black", linewidth = 1) +
+  geom_point(aes(colour = species, shape = year, size = 1.2)) +
+  geom_point(aes(x = number_reads_passing_host_filters_log, colour = species, shape = year, size = 1.2)) +
+  
+  geom_text(aes(label = number_in_pool), hjust = -0.75) +
+  theme_bw() +
+  scale_colour_viridis_d("Species", labels = c("*Ae. canadensis*", "*Ae. vexans*", "*An. earlei*", "*Cq. perturbans*", 
+                                               "*Cx. tarsalis*", "*Oc. dorsalis*", "*Oc. flavescens*", "*Oc. triseriatus*")) +
+  scale_shape_manual("Year", values = c(15, 17, 18, 19)) +
+  theme(panel.grid.major.x  = element_blank(),
+        panel.grid.minor.x  = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        legend.title = element_text(size = 12),
+        legend.text = element_markdown(),
+        legend.position = c(0.95, 0.3),
+        legend.background = element_rect(linetype = 2, linewidth = 0.5, colour = 1)) +
+  guides(colour = guide_legend(override.aes = list(size = 5)),
+         shape = guide_legend(override.aes = list(size = 5)),
+         size = "none") +
+  labs(x = "Total Reads (Log 10)",
+       y = "Sample ID",
+       title = "Reads remaining after host filtering (Mosquito)",
+       subtitle = "Number of specimens represented by each sample displayed to the right")
+
+
+# SAVE PLOT ----
+
+# Saving as a .tiff at 300 dpi. 
+
+tiff(here("Figures/host_filters_plot.tiff"), units="in", width=16, height=10, res=300)
+host_filters_plot
 dev.off()
